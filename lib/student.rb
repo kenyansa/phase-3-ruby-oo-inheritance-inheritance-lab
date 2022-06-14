4c6a7e9a2cfe535439ed7e177cb6106e430aa35e
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 11:44:27 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+    def initialize #initializing with an instance variable, @knowledge, that points to an empty array
+        @knowledge = []
+    end
 
+    def learn (knowledge)
+        @knowledge << knowledge #akes in a string and adds that string to the student's @knowledge array.
+    end
 end
